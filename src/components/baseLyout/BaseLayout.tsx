323e9951c90a3ef667b8b4c5c61f4a3c5ad29a5c
terentyevaolga <terentyeevaolga@gmail.com>
2022-02-16 00:40:59 +0300

    header done

--- src/components/baseLyout/BaseLayout.tsx
+++ src/components/baseLyout/BaseLayout.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './BaseLayout.scss'
-// @ts-ignore
 import logo from '../../images/logo.svg';
+import geoSymbol from '../../images/geo-symbol.svg';
+import signInSymbol from '../../images/signIn-symbol.svg';
 
 export const BaseLayout = () => {
     return (
         <div className="wrapper">
                 <header className="header">
                     <div className="container">
-                        <NavLink to="/"><img src={logo} alt="logo"/></NavLink>
+                        <NavLink to="/" className="header__logo"><img src={logo} alt="logo"/></NavLink>
 
                         <NavLink to="/" className="header__item">Главная</NavLink>
                         <NavLink to="/" className="header__item">Пункты сбора</NavLink>
                         <NavLink to="/" className="header__item">ЭкоМаркет</NavLink>
                         <NavLink to="/" className="header__item">О сервисе</NavLink>
+
+                        <NavLink to="/" className="header__img">
+                            <img src={geoSymbol} className="geo" alt=""/>
+                            Казань
+                        </NavLink>
+
+                        <NavLink to="/" className="header__img">
+                            <img src={signInSymbol} alt=""/>
+                            Войти
+                        </NavLink>
 
 
 
@@ -23,6 +34,7 @@ export const BaseLayout = () => {
                 </header>
 
         </div>
+
 
     )
 }
